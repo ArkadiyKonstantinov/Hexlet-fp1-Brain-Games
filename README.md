b9d876912df580da3fc90314d08fc0e18273b3f6
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-06-12 19:39:02 +0300

    Complete stage 5

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 [![Actions Status](https://github.com/ArkadiyKonstantinov/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/ArkadiyKonstantinov/frontend-project-lvl1/actions)
 ### CodeClimate Maintainability Badge
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
+
+### Asclinema for Brain-Even:
+[![asciicast](https://asciinema.org/a/3xifb2btDtVPu3lGd2P9tKuqG.svg)](https://asciinema.org/a/3xifb2btDtVPu3lGd2P9tKuqG)
---
+++ src/even.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+const getRandomInt = () => {
+  const min = 1;
+  const max = 100;
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const gameEven = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const answersToWin = 3;
+  let rightAnswersCount = 0;
+
+  do {
+    const number = getRandomInt();
+    const rightAnswer = (number % 2 === 0) ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+      rightAnswersCount += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      rightAnswersCount = 0;
+    }
+  } while (rightAnswersCount < answersToWin);
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default gameEven;
