2c5ae4822966d9105271458e370d1e1c24fa039f
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-07 13:21:04 +0300

    Refactoring engine

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const start = (game, gameRule) => {
+const start = (game) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  gameRule();
-  for (let i = 1; i <= 3; i += 1) {
-    const [question, rightAnswer] = game();
+  console.log(game.description());
+
+  const roundCountToWin = 3;
+  for (let i = 1; i <= roundCountToWin; i += 1) {
+    const [question, rightAnswer] = game.generateRound();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === rightAnswer.toString()) {
-      console.log('Correct!');
-      if (i === 3) {
-        console.log(`Congratulations, ${userName}!`);
-      }
-    } else {
+    if (userAnswer !== rightAnswer.toString()) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-      break;
+      return;
     }
+    console.log('Correct!');
   }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default start;
