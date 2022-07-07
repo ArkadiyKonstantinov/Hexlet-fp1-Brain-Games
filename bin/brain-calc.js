1361ccd0ac4efa826de88917f9cdda0a698883bf
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-07 13:37:03 +0300

    Refactoring brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import start from '../src/index.js';
-import { calc, calcRule } from '../src/games/calc.js';
+import * as gameCalc from '../src/games/calc.js';
 
-start(calc, calcRule);
+start(gameCalc);
--- src/games/calc.js
+++ src/games/calc.js
@@ -1,8 +1,8 @@
 import getRandomInt from '../utils.js';
 
-export const calcRule = () => console.log('What is the result of the expression?');
+export const description = () => 'What is the result of the expression?';
 
-export const calc = () => {
+export const generateRound = () => {
   const number1 = getRandomInt(1, 100);
   const number2 = getRandomInt(1, 100);
   const operators = ['+', '-', '*'];
