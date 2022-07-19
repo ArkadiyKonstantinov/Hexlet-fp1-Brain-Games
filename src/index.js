ad9898b998d574a461f3a924258163f127c6501c
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-19 15:43:10 +0300

    Change wins counter name

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const winsCount = 3;
+
 const start = (game) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(game.description);
 
-  const roundCountToWin = 3;
-  for (let i = 1; i <= roundCountToWin; i += 1) {
+  for (let i = 1; i <= winsCount; i += 1) {
     const [question, rightAnswer] = game.generateRound();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
