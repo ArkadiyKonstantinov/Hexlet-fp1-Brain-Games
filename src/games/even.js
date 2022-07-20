9ad71bdb255121be3d6575c4bddb9ae8048578fd
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-20 20:29:07 +0300

    Refactoring brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,12 @@
 import getRandomInt from '../utils.js';
 
+const isEven = (num) => (num % 2 === 0);
+
 export const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export const generateRound = () => {
   const number = getRandomInt(1, 100);
-  const rightAnswer = (number % 2 === 0) ? 'yes' : 'no';
   const question = String(number);
+  const rightAnswer = isEven(number) ? 'yes' : 'no';
   return [question, rightAnswer];
 };
