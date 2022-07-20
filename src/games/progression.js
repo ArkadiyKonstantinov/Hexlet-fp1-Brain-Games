2f33d23c0a3fa0613f5b3369abd8e127fe0a147b
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-20 21:12:20 +0300

    Refactoring brain-progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,26 +1,24 @@
 import getRandomInt from '../utils.js';
 
+const progressionLength = 10;
+
+const getProgression = (startNum, increment, missingNumberPosition, length) => {
+  const result = [startNum];
+  for (let i = 0; i < length; i += 1) {
+    result.push(result[i] + increment);
+  }
+  result[missingNumberPosition - 1] = '..';
+  return result.join(' ');
+};
+
 export const description = 'What number is missing in the progression?';
 
 export const generateRound = () => {
-  let number = getRandomInt(1, 19);
+  const startNumber = getRandomInt(1, 19);
   const increment = getRandomInt(1, 10);
   const missingNumberPosition = getRandomInt(1, 10);
-  let question = '';
-  let rightAnswer = 0;
-
-  for (let i = 1; i <= 10; i += 1) {
-    if (i === missingNumberPosition) {
-      question = `${question} ..`;
-      rightAnswer = number;
-      number += increment;
-    } else {
-      question = `${question} ${number}`;
-      number += increment;
-    }
-  }
-  question = question.trim();
-  rightAnswer = String(rightAnswer);
+  const question = getProgression(startNumber, increment, missingNumberPosition, progressionLength);
+  const rightAnswer = String(startNumber + (increment * (missingNumberPosition - 1)));
 
   return [question, rightAnswer];
 };
