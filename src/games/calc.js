953e165eaf1950897a0b13d12825e803b9bffa92
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-20 16:05:27 +0300

    Refactoring brain-calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,24 +1,37 @@
 import getRandomInt from '../utils.js';
 
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  const index = getRandomInt(0, operators.length - 1);
+  return operators[index];
+};
+
+const calculate = (operator, num1, num2) => {
+  let result = '';
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      result = NaN;
+      break;
+  }
+  return result;
+};
+
 export const description = 'What is the result of the expression?';
 
 export const generateRound = () => {
   const number1 = getRandomInt(1, 100);
   const number2 = getRandomInt(1, 100);
-  const operators = ['+', '-', '*'];
-  const operator = operators[getRandomInt(0, operators.length - 1)];
-  let rightAnswer = '';
-  switch (operator) {
-    case '+':
-      rightAnswer = String(number1 + number2);
-      break;
-    case '-':
-      rightAnswer = String(number1 - number2);
-      break;
-    default:
-      rightAnswer = String(number1 * number2);
-      break;
-  }
+  const operator = getRandomOperator();
   const question = `${number1} ${operator} ${number2}`;
+  const rightAnswer = String(calculate(operator, number1, number2));
   return [question, rightAnswer];
 };
