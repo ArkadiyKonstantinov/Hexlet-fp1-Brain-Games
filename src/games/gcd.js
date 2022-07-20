6ceb71f6af37a2c1e7f55d474067b6ec9dd9ccc9
Arkadiy Konstantinov <arkadiykonst@outlook.com>
2022-07-20 21:26:21 +0300

    Refactoring brain-gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,10 +1,20 @@
 import getRandomInt from '../utils.js';
 
 const calcGcd = (a, b) => {
-  if (b) {
-    return calcGcd(b, a % b);
+  let num1 = 0;
+  let num2 = 0;
+  if (a >= b) {
+    num1 = a;
+    num2 = b;
+  } else {
+    num1 = b;
+    num2 = a;
   }
-  return String(a);
+  if (!num2) {
+    return num1;
+  }
+
+  return calcGcd(num2, num1 % num2);
 };
 
 export const description = 'Find the greatest common divisor of given numbers.';
@@ -13,6 +23,6 @@ export const generateRound = () => {
   const number1 = getRandomInt(1, 10);
   const number2 = getRandomInt(1, 10);
   const question = `${number1} ${number2}`;
-  const rightAnswer = (number1 > number2) ? (calcGcd(number1, number2)) : calcGcd(number2, number1);
+  const rightAnswer = String(calcGcd(number1, number2));
   return [question, rightAnswer];
 };
